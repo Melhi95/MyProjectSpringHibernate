d56642fa08b7f1251040422d9a730663cf3eedc5
Melhi95 <melhi.92@mail.ru>
2024-12-12 05:09:37 +0300

    Типизация Query в CarDaoImp.java и удаление избыточной типизации в return

--- src/main/java/ru/vaidom/dao/CarDaoImp.java
+++ src/main/java/ru/vaidom/dao/CarDaoImp.java
@@ -5,7 +5,6 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.vaidom.model.Car;
-import ru.vaidom.model.User;
 
 @Repository
 public class CarDaoImp implements CarDao {
@@ -15,23 +14,23 @@ public class CarDaoImp implements CarDao {
 
     @Override
     public Car getCarBySeries(int series) {
-        Query query = sessionFactory.getCurrentSession().createQuery("from Car c where c.series = :series");
+        Query<Car> query = sessionFactory.getCurrentSession().createQuery("from Car c where c.series = :series", Car.class);
         query.setParameter("series", series);
-        return (Car) query.uniqueResult();
+        return query.uniqueResult();
     }
 
     @Override
     public Car getCarByModel(String model) {
-        Query query = sessionFactory.getCurrentSession().createQuery("from Car c where c.model = :model");
+        Query<Car> query = sessionFactory.getCurrentSession().createQuery("from Car c where c.model = :model", Car.class);
         query.setParameter("model", model);
-        return (Car) query.uniqueResult();
+        return query.uniqueResult();
     }
 
     @Override
     public Car getCarByModelAndSeries(String model, int series) {
-        Query query = sessionFactory.getCurrentSession().createQuery("from Car c where c.model = :model and c.series = :series");
+        Query<Car> query = sessionFactory.getCurrentSession().createQuery("from Car c where c.model = :model and c.series = :series", Car.class);
         query.setParameter("model", model);
         query.setParameter("series", series);
-        return (Car) query.uniqueResult();
+        return query.uniqueResult();
     }
 }
